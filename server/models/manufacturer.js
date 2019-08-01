9e0bf95f63014d66cd3e853a28a12f173c294725
zAndrey95 <35850949+zAndrey95@users.noreply.github.com>
2019-08-01 14:53:02 +0300

    Revert "all in"
    This reverts commit 21453132eebc661824411059ba412a5b1eb23196.

--- server/models/manufacturer.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const manufacturerSchema = new Schema({
-    name: String,
-    country: String,
-});
-
-module.exports = mongoose.model('manufacturers', manufacturerSchema);
